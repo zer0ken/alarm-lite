bfc51edfe363db74fd7e495d73c34e13cfa323ee
zer0ken <a37592777@gmail.com>
2024-05-27 23:10:06 +0900

    알람 목록 컴포저블 함수 및 관련 요소

---
+++ app/src/main/java/com/example/alarmapp/view/IconButton.kt
@@ -0,0 +1,39 @@
+package com.example.alarmapp.view
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+
+/**
+ * IconToggleButton을 생성하는 컴포지션 함수입니다.
+ *
+ * @param painter 내부에 삽입될 아이콘
+ * @param checked 초기 버튼 상태
+ * @param onCheckedChange 버튼 상태가 변경될 때 호출되는 콜백 함수
+ * @param modifier 버튼의 모양과 크기 조정
+ * @author 이현령
+ */
+@Composable
+fun IconToggleButton_(
+    painter: Painter,
+    checked: Boolean = false,
+    onCheckedChange: (Boolean) -> Unit = {},
+    buttonModifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    IconToggleButton(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        modifier = buttonModifier.size(20.dp)
+    ) {
+        Icon(
+            painter = painter,
+            contentDescription = "",
+            modifier = iconModifier.size(10.dp)
+        )
+    }
+}
