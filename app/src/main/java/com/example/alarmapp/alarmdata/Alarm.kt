05c90755941ec3b97515e597ee7255698a53e456
MaBeing <john9912@naver.com>
2024-05-27 15:52:59 +0900

    JK Alarm, AlarmGroup edit

--- app/src/main/java/com/example/alarmapp/alarmdata/Alarm.kt
+++ app/src/main/java/com/example/alarmapp/alarmdata/Alarm.kt
@@ -1,8 +1,10 @@
 package com.example.alarmapp.alarmdata
 
 data class Alarm (
-    var content: String,
-    var hour: Int,          // 00 ~ 23
-    var minute: Int,        // 00 ~ 59
-    var isOn: Boolean
+    var content: String = "",       // 알람 설명(빈 문자열 가능)
+    var hour: Int,                  // 시각(00 ~ 23)
+    var minute: Int,                // 분(00 ~ 59)
+    var isOn: Boolean = true,       // 알람의 켜짐/꺼짐 여부(기본값: true)
+    var bookmark: Boolean = false,  // 즐겨찾기(기본값: false)
+    var groupName: String = ""      // 속한 그룹의 이름(빈 문자열의 경우 소속된 그룹 없음)
 )
--- app/src/main/java/com/example/alarmapp/alarmdata/AlarmGroup.kt
+++ app/src/main/java/com/example/alarmapp/alarmdata/AlarmGroup.kt
@@ -1,5 +1,8 @@
 package com.example.alarmapp.alarmdata
 
-class AlarmGroup {
+data class AlarmGroup (
+    var groupName: String = "",     // 그룹 이름(그룹을 구분하는 key값, 같은 그룹 이름이 존재하면 안됨)
+    var bookmark: Boolean = false   // 즐겨찾기(기본값: false)
+){
 
 }
