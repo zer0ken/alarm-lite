10f4dd2e51699fcc79e466ba63a07bd9ac13b369
MaBeing <john9912@naver.com>
2024-05-28 14:17:58 +0900

    JK implement(remove list in Group, add some functions, not completed)

--- app/src/main/java/com/example/alarmapp/alarmdata/Alarm.kt
+++ app/src/main/java/com/example/alarmapp/alarmdata/Alarm.kt
@@ -6,5 +6,6 @@ data class Alarm (
     var minute: Int,                // 분(00 ~ 59)
     var isOn: Boolean = true,       // 알람의 켜짐/꺼짐 여부(기본값: true)
     var bookmark: Boolean = false,  // 즐겨찾기(기본값: false)
-    var groupName: String = ""      // 속한 그룹의 이름(빈 문자열의 경우 소속된 그룹 없음, 기본값: "")
+    var groupName: String = "",     // 속한 그룹의 이름(빈 문자열의 경우 소속된 그룹 없음, 기본값: "")
+    var updatedTime: Long           // 알람이 생성/수정된 시스템 시간을 저장(사용하지 않는 알람 자동 삭제 시 사용)
 )
--- app/src/main/java/com/example/alarmapp/alarmdata/AlarmManager.kt
+++ app/src/main/java/com/example/alarmapp/alarmdata/AlarmManager.kt
@@ -1,12 +1,16 @@
 package com.example.alarmapp.alarmdata
 
-// 전반적인 알람의 관리를 담당하는 클래스(추가, 삭제, 편집 시 데이터의 무결성 보장)
-// UI를 통한 모든 행동은 이 클래스를 통해 사용
+// 전반적인 알람의 관리를 담당하는 클래스(추가, 삭제, 편집 시 데이터의 무결성을 보장해야 함)
+// 최종 "확인" 버튼 등의 행동은 이 클래스를 통해 사용
 // ex1. 알람 추가 버튼(+) 클릭 후 모든 설정을 마치고 확인 버튼을 누를 시 -> AlarmManager.addAlarm(a) (a는 alarm)
 // ex2. 화면에 표시되는 첫 번째 알람 삭제 시 -> AlarmManager.removeAlarm(0)
+
+// 알람의 경우 List의 index를 통해 접근
+// 그룹의 경우 getGroup(name)을 통해 객체에 접근
 object AlarmManager {
-    private val alarmList = mutableListOf<Alarm>()          // 메인 화면에 표시되는 알람 List(이 순서가 "메인" 화면에 반영됨)
-    private val alarmGroupSet = mutableSetOf<AlarmGroup>()  // 생성된 모든 그룹을 담는 Set(이 순서가 "드롭다운 리스트"에 반영됨, 그룹 이름이 같으면 동등한 그룹)
+    val alarmList = mutableListOf<Alarm>()          // 메인 화면에 표시되는 알람 List(이 순서가 "메인" 화면에 반영됨)
+    val alarmGroupSet = mutableSetOf<AlarmGroup>()  // 생성된 모든 그룹을 담는 Set(이 순서가 "드롭다운 리스트"에 반영됨, 그룹 이름이 같으면 동등한 그룹)
+    val alarmGroupMap = mutableMapOf<String,AlarmGroup>()   // 그룹 이름을 key, AlarmGroup을 value로 갖는 Map
 
     // "알람 추가" 화면에서 최종적으로 "확인" 버튼 클릭 시 호출
     fun addAlarm(alarm: Alarm) {
@@ -40,14 +44,26 @@ object AlarmManager {
         alarm.bookmark = !alarm.bookmark
     }
 
+    // 그룹 이름(key)를 통해 "AlarmGroup"(value) 객체를 반환
+    // 해당 그룹 이름이 없으면 null 반환
+    fun getGroup(name: String): AlarmGroup? {
+        return alarmGroupMap.get(name)
+    }
 
-    // "알람 그룹 추가" 화면에서 최종적으로 "확인(그룹 추가)" 버튼 클릭 시 호출
+    // "알람 그룹 추가" 화면에서 최종적으로 "확인(그룹 추가)" 버튼 클릭 시 호출(그룹 이름이 같으면 Set에 추가, Map의 경우 value가 대체됨)
     fun addGroup(alarmGroup: AlarmGroup) {
         alarmGroupSet.add(alarmGroup)
+        alarmGroupMap[alarmGroup.groupName] = alarmGroup
     }
 
     // "알람 그룹 편집" 화면에서 최종적으로 "확인(그룹 이름 변경)" 버튼 클릭 시 호출(현재 알람 그룹 편집의 경우 알람 그룹 이름을 변경하는 기능뿐임)
     fun editGroup(alarmGroup: AlarmGroup, name: String) {
         alarmGroup.changeGroupName(name.trim())
+    }
+
+    // "알람 그룹 삭제" 화면에서 최종적으로 "확인(삭제)" 버튼 클릭 시 호출
+    fun removeGroup(name: String) {
+        alarmGroupSet.remove(getGroup(name))
+        alarmGroupMap.remove(name)
     }
 }
