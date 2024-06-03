c34c0ba07252ae210378c67d6394362ce83b0695
MaBeing <john9912@naver.com>
2024-06-03 12:48:12 +0900

    AlarmManager에서 Set을 주석 처리 및 정렬 기능 메서드 추가

--- app/src/main/java/com/example/alarmapp/alarmdata/AlarmComparator.kt
+++ app/src/main/java/com/example/alarmapp/alarmdata/AlarmComparator.kt
@@ -7,7 +7,7 @@ import kotlin.Comparator
 object AlarmComparator {
 
     // 상대적인 시간 기준 Comparator
-    val byNextAlarmTime: Comparator<Alarm> = Comparator { a1, a2 ->
+    val relative: Comparator<Alarm> = Comparator { a1, a2 ->
         val now = Calendar.getInstance()
 
         // 다음 울릴 시간 계산
@@ -23,7 +23,7 @@ object AlarmComparator {
     }
 
     // 절대적인 시간 기준 Comparator
-    val byHourMinute: Comparator<Alarm> = Comparator { a1, a2 ->
+    val absolute: Comparator<Alarm> = Comparator { a1, a2 ->
         val hourCompare = a1.hour.compareTo(a2.hour)
         if (hourCompare == 0) {
             a1.minute.compareTo(a2.minute)
@@ -61,12 +61,12 @@ fun main() {
     // 절대 시간 기준
     // 1. 알람의 켜짐/꺼짐은 고려하지 않음
     // 2. 00시 00분이 가장 위, 23시 59분이 가장 아래
-    alarms.sortWith(AlarmComparator.byHourMinute)
+    alarms.sortWith(AlarmComparator.absolute)
     println("Sorted by hour and minute: $alarms")
 
     // 상대 시간 기준
     // 1. 켜진 알람을 우선으로 함
     // 2. 현재 시간으로부터 (미래로) 가장 가까운 시간
-    alarms.sortWith(AlarmComparator.byNextAlarmTime)
+    alarms.sortWith(AlarmComparator.relative)
     println("Sorted by next alarm time: $alarms")
 }
