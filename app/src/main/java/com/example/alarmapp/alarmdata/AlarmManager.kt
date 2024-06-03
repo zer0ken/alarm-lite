dead9614bd03a8aa8cb9b6662d8cd8e1b4f11421
MaBeing <john9912@naver.com>
2024-06-03 18:40:21 +0900

    AlarmManager에서 editAlarm 삭제(주석 참고), id를 통한 Alarm 객체 얻는 함수 추가

--- app/src/main/java/com/example/alarmapp/alarmdata/AlarmManager.kt
+++ app/src/main/java/com/example/alarmapp/alarmdata/AlarmManager.kt
@@ -2,7 +2,7 @@ package com.example.alarmapp.alarmdata
 
 /*
 전반적인 알람의 관리를 담당하는 클래스(추가, 삭제, 편집 시 데이터의 무결성을 보장해야 함)
-최종 "확인" 버튼 등의 행동은 이 클래스를 통해 사용
+※ alarmList와 alarmGroup에 직접 추가 및 삭제를 진행하지 말 것 (이 클래스에 정의된 함수를 사용해야 함, 알람 편집 시에는 getAlarm()을 통해 얻은 객체에서 값을 조정
 ex1. 알람 추가 버튼(+) 클릭 후 모든 설정을 마치고 확인 버튼을 누를 시 -> AlarmManager.addAlarm(a) (a는 alarm)
 ex2. 화면에 표시되는 첫 번째 알람 삭제 시 -> AlarmManager.removeAlarm(0)
 ex3. 알람 그룹 "학교" 삭제 시 -> AlarmManager.removeGroup("학교")
@@ -12,50 +12,60 @@ ex3. 알람 그룹 "학교" 삭제 시 -> AlarmManager.removeGroup("학교")
 // 그룹의 경우 getGroup(name)을 통해 객체에 접근
 object AlarmManager {
     val alarmList = mutableListOf<Alarm>()          // 메인 화면에 표시되는 알람 List(이 순서가 "메인" 화면에 반영됨)
-//    val alarmGroupSet =
-//        mutableSetOf<AlarmGroup>()  // 생성된 모든 그룹을 담는 Set(이 순서가 "드롭다운 리스트"에 반영됨, 그룹 이름이 같으면 동등한 그룹)
-    val alarmGroupMap =
-        mutableMapOf<String, AlarmGroup>()   // 그룹 이름을 key, AlarmGroup을 value로 갖는 Map
+    val alarmGroupMap = mutableMapOf<String, AlarmGroup>()   // 그룹 이름을 key, AlarmGroup을 value로 갖는 Map
 
-    // alarmList의 index를 통해 "Alarm" 객체를 반환
-    fun getAlarm(index: Int): Alarm {
+    // Alarm의 고유 id를 통해 객체 얻기
+    // 해당 id를 가진 알람이 없다면 null 반환
+    fun getAlarm(id: Int): Alarm? {
+        return alarmList.find { it.id == id }
+    }
+
+    // alarmList의 index를 통해 객체 얻기
+    fun getAlarmFromIndex(index: Int): Alarm {
         return alarmList[index]
     }
 
     // "알람 추가" 화면에서 최종적으로 "확인" 버튼 클릭 시 호출
     // 만약 추가하고자하는 "Alarm" 객체가 가진 그룹 이름이 alarmGroupSet에 존재하지 않는 경우, 해당 그룹 이름으로 새로운 그룹을 생성함
     fun addAlarm(alarm: Alarm) {
-        val insertedAlarmGroup: AlarmGroup? = getGroup(alarm.groupName)
-        if (insertedAlarmGroup == null) {
+        if (getGroup(alarm.groupName) == null) {
             addGroup(AlarmGroup(alarm.groupName))
         }
         alarmList.add(alarm)
     }
 
-    // "알람 편집" 화면에서 최종적으로 "확인" 버튼 클릭 시 호출
-    fun editAlarm(index: Int, alarm: Alarm) {
-        alarmList[index] = alarm
+    // Alarm의 고유 id를 통한 알람 삭제
+    // 삭제된 "Alarm" 객체를 반환
+    fun removeAlarm(id: Int): Alarm? {
+        val alarm = alarmList.find { it.id == id }
+        return if (alarm != null) {
+            alarmList.remove(alarm)
+            alarm
+        } else {
+            null
+        }
     }
 
-    // "메인" 화면에서 해당 알림 "삭제" 버튼 클릭 시 호출(삭제한 알람 객체 반환)
-    fun removeAlarm(index: Int): Alarm {
+    // alarmList의 index를 통한 알람 삭제
+    // 삭제된 "Alarm" 객체를 반환
+    fun removeAlarmFromIndex(index: Int): Alarm {
         return alarmList.removeAt(index)
     }
 
-    // "메인" 화면에서 "전체 삭제" 버튼 클릭 시 호출(되돌리기 기능을 위한 반환값 추가 예정)
-    fun removeAllAlarm() {
+    // "메인" 화면에서 "전체 삭제" 버튼 클릭 시 호출(되돌리기 기능을 위해 List를 복사하여 반환)
+    fun removeAllAlarm(): List<Alarm> {
+        val removedAlarms = alarmList.toList()
         alarmList.clear()
+        return removedAlarms
     }
 
     // "메인" 화면에서 "ON/OFF" 스위치 클릭 시 호출
-    fun toggleOnOff(index: Int) {
-        val alarm: Alarm = alarmList[index]
+    fun toggleOnOff(alarm: Alarm) {
         alarm.isOn = !alarm.isOn
     }
 
     // "메인" 화면에서 "즐겨찾기" 버튼 클릭 시 호출
-    fun toggleBookmark(index: Int) {
-        val alarm: Alarm = alarmList[index]
+    fun toggleBookmark(alarm: Alarm) {
         alarm.bookmark = !alarm.bookmark
     }
 
@@ -67,7 +77,6 @@ object AlarmManager {
 
     // "알람 그룹 추가" 화면에서 최종적으로 "확인(그룹 추가)" 버튼 클릭 시 호출(그룹 이름이 같으면 Set에 추가, Map의 경우 value가 대체됨)
     fun addGroup(alarmGroup: AlarmGroup) {
-//        alarmGroupSet.add(alarmGroup)
         alarmGroupMap[alarmGroup.groupName] = alarmGroup
     }
 
@@ -87,7 +96,6 @@ object AlarmManager {
                 item.groupName = ""
         }
         // Map에서 삭제
-//        alarmGroupSet.remove(getGroup(name))
         alarmGroupMap.remove(name)
         return removedGroup
     }
---
+++ app/src/main/java/com/example/alarmapp/alarmdata/AlarmViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.alarmapp.alarmdata
+
+import androidx.lifecycle.ViewModel
+
+class AlarmViewModel: ViewModel() {
+
+}
