aa543f97211775ec3265229f3d768094122293a6
flffkd12 <flffkd12@gmail.com>
2024-06-15 17:05:08 +0900

    Lint 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
 
     kapt("androidx.room:room-runtime:2.6.1")
+    //noinspection KaptUsageInsteadOfKsp
     kapt("androidx.room:room-compiler:2.6.1")
     kapt("androidx.room:room-ktx:2.6.1")
 
