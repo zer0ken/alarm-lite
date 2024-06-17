c065afba29fa688afe169cd2e9e4624160cce796
zer0ken <a37592777@gmail.com>
2024-06-18 01:32:51 +0900

    top app bar에 선택 모드 표시 추가

--- app/src/main/java/com/example/alarmapp/screen/MainScreen.kt
+++ app/src/main/java/com/example/alarmapp/screen/MainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -25,6 +26,7 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -88,6 +90,11 @@ fun MainScreen(navController: NavController, mainViewModel: MainViewModel) {
             alarms.sortedWith(AlarmComparator.relative)
         }
     }
+
+    LaunchedEffect(mainViewModel.isSelectMode) {
+        scrollBehavior.state.heightOffset = -1000f
+    }
+
     var firstText by remember { mutableStateOf("") }
     var secondText by remember { mutableStateOf("") }
     if (alarms.isEmpty()) {
@@ -118,7 +125,11 @@ fun MainScreen(navController: NavController, mainViewModel: MainViewModel) {
                 }
             }
             val formatter = DateTimeFormatter.ofPattern("M월 d일 (E)", Locale.forLanguageTag("ko"))
-            val formattedTime = mainViewModel.formatTime(alarmTime.hour, alarmTime.minute, mainViewModel.is24HourView)
+            val formattedTime = mainViewModel.formatTime(
+                alarmTime.hour,
+                alarmTime.minute,
+                mainViewModel.is24HourView
+            )
             secondText = "${alarmTime.format(formatter)} $formattedTime"
         }
     }
@@ -128,122 +139,141 @@ fun MainScreen(navController: NavController, mainViewModel: MainViewModel) {
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            LargeTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                expandedHeight = 280.dp,
-                title = {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = if (scrollBehavior.state.collapsedFraction < 0.5f) {
-                            BiasAlignment.Horizontal(-scrollBehavior.state.collapsedFraction)
-                        } else {
-                            Alignment.Start
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 8.dp, end = 24.dp)
-                            .height(280.dp)
-                    ) {
-                        if (scrollBehavior.state.collapsedFraction < 0.5f) {
-                            Text(
-                                text = firstText,
-                                maxLines = 2,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 34.sp,
-                                textAlign = TextAlign.Center,
-                            )
-                            Text(
-                                text = secondText,
-                                fontSize = 20.sp
-                            )
-                        } else {
-                            Text(
-                                text = "알람 목록",
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 24.sp,
+            if (!mainViewModel.isSelectMode) {
+                LargeTopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    expandedHeight = 280.dp,
+                    title = {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = if (scrollBehavior.state.collapsedFraction < 0.5f) {
+                                BiasAlignment.Horizontal(-scrollBehavior.state.collapsedFraction)
+                            } else {
+                                Alignment.Start
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 8.dp, end = 24.dp)
+                                .height(280.dp)
+                        ) {
+                            if (scrollBehavior.state.collapsedFraction < 0.5f) {
+                                Text(
+                                    text = firstText,
+                                    maxLines = 2,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 34.sp,
+                                    textAlign = TextAlign.Center,
+                                )
+                                Text(
+                                    text = secondText,
+                                    fontSize = 20.sp
+                                )
+                            } else {
+                                Text(
+                                    text = "알람 목록",
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 24.sp,
+                                )
+                            }
+                        }
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { menuExpanded = true }) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.baseline_sort_24),
+                                contentDescription = "Sort"
                             )
                         }
-                    }
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = { menuExpanded = true }) {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(R.drawable.baseline_sort_24),
-                            contentDescription = "Sort"
-                        )
-                    }
-                    DropdownMenu(
-                        expanded = menuExpanded,
-                        onDismissRequest = { menuExpanded = false },
-                        modifier = Modifier
-                            .width(128.dp)
-                    ) {
-                        sortList.forEach { sortItem ->
-                            DropdownMenuItem(
-                                text = {
-                                    Row(
-                                        horizontalArrangement = Arrangement.SpaceBetween,
-                                        verticalAlignment = Alignment.CenterVertically,
-                                        modifier = Modifier.fillMaxWidth()
-                                    ) {
-                                        Text(
-                                            text = sortItem,
-                                            color = if (sortItem == mainViewModel.selectedSort) Color(
-                                                0xFF734D4D
-                                            ) else Color.Black
-                                        )
-                                        if (sortItem == mainViewModel.selectedSort) {
-                                            Icon(
-                                                imageVector = Icons.Default.Check,
-                                                contentDescription = "Selected sort",
-                                                tint = Color(0xFF734D4D),
-                                                modifier = Modifier
-                                                    .size(20.dp)
+                        DropdownMenu(
+                            expanded = menuExpanded,
+                            onDismissRequest = { menuExpanded = false },
+                            modifier = Modifier
+                                .width(128.dp)
+                        ) {
+                            sortList.forEach { sortItem ->
+                                DropdownMenuItem(
+                                    text = {
+                                        Row(
+                                            horizontalArrangement = Arrangement.SpaceBetween,
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            modifier = Modifier.fillMaxWidth()
+                                        ) {
+                                            Text(
+                                                text = sortItem,
+                                                color = if (sortItem == mainViewModel.selectedSort) Color(
+                                                    0xFF734D4D
+                                                ) else Color.Black
                                             )
+                                            if (sortItem == mainViewModel.selectedSort) {
+                                                Icon(
+                                                    imageVector = Icons.Default.Check,
+                                                    contentDescription = "Selected sort",
+                                                    tint = Color(0xFF734D4D),
+                                                    modifier = Modifier
+                                                        .size(20.dp)
+                                                )
+                                            }
+                                        }
+                                    },
+                                    onClick = {
+                                        mainViewModel.selectedSort = sortItem
+                                        menuExpanded = false
+                                        sortedAlarms = if (mainViewModel.selectedSort == "시간순") {
+                                            alarms.sortedWith(AlarmComparator.absolute)
+                                        } else {
+                                            alarms.sortedWith(AlarmComparator.relative)
                                         }
                                     }
-                                },
-                                onClick = {
-                                    mainViewModel.selectedSort = sortItem
-                                    menuExpanded = false
-                                    sortedAlarms = if (mainViewModel.selectedSort == "시간순") {
-                                        alarms.sortedWith(AlarmComparator.absolute)
-                                    } else {
-                                        alarms.sortedWith(AlarmComparator.relative)
-                                    }
-                                }
+                                )
+                            }
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            navController.navigate(Routes.CreateAlarm.route) {
+                                popUpTo(Routes.MainScreen.route) { inclusive = false }
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = "Add Alarm"
+                            )
+                        }
+                        IconButton(onClick = {
+                            navController.navigate(Routes.Setting.route) {
+                                popUpTo(Routes.MainScreen.route) { inclusive = false }
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Settings,
+                                contentDescription = "Setting"
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior,
+                )
+            } else {
+                TopAppBar(
+                    title = { Text(text = "${mainViewModel.getSelectedAlarms().size}개 선택됨") },
+                    actions = {
+                        IconButton(onClick = {
+                            mainViewModel.getSelectedAlarms().values.forEach {
+                                it.isSelected = false
+                            }
+                            mainViewModel.isSelectMode = false
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Clear,
+                                contentDescription = "선택 취소"
                             )
                         }
                     }
-                },
-                actions = {
-                    IconButton(onClick = {
-                        navController.navigate(Routes.CreateAlarm.route) {
-                            popUpTo(Routes.MainScreen.route) { inclusive = false }
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Add,
-                            contentDescription = "Add Alarm"
-                        )
-                    }
-                    IconButton(onClick = {
-                        navController.navigate(Routes.Setting.route) {
-                            popUpTo(Routes.MainScreen.route) { inclusive = false }
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Settings,
-                            contentDescription = "Setting"
-                        )
-                    }
-                },
-                scrollBehavior = scrollBehavior,
-            )
+                )
+            }
         },
         bottomBar = {
             if (mainViewModel.isSelectMode) {
